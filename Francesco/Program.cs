ae3923f0bbe0336440d3b5c2ebe385a93e487150
francesco marinelli <francescomarinelli04@gmail.com>
2020-10-14 08:44:18 +0200

    Aggiungere i file di progetto.

---
+++ Francesco/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Francesco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num = 1024, resto;
+            string binario = "";
+            while (Num > 0)
+            {
+                resto = Num % 2;
+                Num /= 2;
+                binario = Convert.ToString(resto) + binario;
+            }
+            Console.WriteLine($"il numero è {binario}");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
