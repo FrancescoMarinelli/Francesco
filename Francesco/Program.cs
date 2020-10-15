cb6c314d61e20a9bd49696af8e3df2b9a1008807
francesco marinelli <francescomarinelli04@gmail.com>
2020-10-15 09:14:23 +0200

    Algoritmo che converte in qualsiasi base un numero decimale

--- Francesco/Program.cs
+++ Francesco/Program.cs
@@ -6,17 +6,48 @@ namespace Francesco
     {
         static void Main(string[] args)
         {
-            int resto, ottagonale;
-            string binario = "";
+            int resto, numero, valorebase;
+            string numeroconvertito = "";
+           
+            Console.WriteLine("inserisci la base in cui vuoi convertire");
+            valorebase = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("inserisci il numero da convertire");
-            ottagonale = Convert.ToInt32(Console.ReadLine());
-           while(ottagonale > 0)
+            numero = Convert.ToInt32(Console.ReadLine());
+           while(numero > 0)
             {
-                resto = ottagonale % 8;
-                ottagonale = ottagonale / 8;
-                binario = Convert.ToString(resto) + binario;
+                resto = numero % valorebase;
+                if(resto == 15)
+                {
+                  numeroconvertito += "f";
+                }
+                else if(resto ==14)
+                {
+                    numeroconvertito += "e";
+                }
+                else if (resto == 13)
+                {
+                    numeroconvertito += "d";
+                }
+                else if (resto == 12)
+                {
+                    numeroconvertito += "c";
+                }
+                else if (resto == 11)
+                {
+                    numeroconvertito += "b";
+                }
+                else if (resto == 10)
+                {
+                    numeroconvertito += "a";
+                }
+                else
+                {
+                    numeroconvertito = Convert.ToString(resto) + numeroconvertito;
+                }
+                numero = numero / valorebase;
+               // numeroconvertito = Convert.ToString(resto) + numeroconvertito; 
             }
-            Console.WriteLine($"il numero binario è {binario}");
+            Console.WriteLine($"il numero convertito in base {valorebase} è: {numeroconvertito}");
             Console.ReadLine();
    
             
